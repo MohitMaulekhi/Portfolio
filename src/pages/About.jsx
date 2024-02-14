7dee5a5e6ac3c268cdcc7c8dc739b72501eb6416
Mohit Maulekhi <mohitmaulekhi312@gmail.com>
2024-02-14 19:02:32 +0530

    Image url setup

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -4,11 +4,11 @@ function About() {
     <Container element = {
       <div className="my-[5vh] ml-[10vw]">
         <div className="flex">
-        <img src="src\assets\Avatar.png" alt="not found" />
+        <img src="https://res.cloudinary.com/mohit-learning-backend/image/upload/v1707917504/Assests%20for%20Porfolio/wskryrnv9yxgnngqgyv2.png" alt="not found" />
         <div className="self-center text-[4vmin] font-sans italic w-[30vw]">
           A devloper who codes in dark to avoid <span className="text-blue-700">bugs</span>.</div>
         </div>
-        <div className="ml-[15vw] mt-[2vh] mr-[7.5vw]"> I am pursuing software engineer from Netaji Subhas University of Technology. I enjoy creating things that live on the internet, whether that be websites, applications, or anything in between. I have been freelancing for a year now while studying at the university and I've manage to gain a decent amount of experience and valuable knowledge from all different kinds of fields throughout my projects/work.</div>
+        <div className="ml-[15vw] mt-[2vh] mr-[7.5vw]"> I am pursuing software engineer from Netaji Subhas University of Technology.I enjoy creating things that live on the internet, whether that be websites, applications, or anything in between. I have been freelancing for a year now while studying at the university and I've manage to gain a decent amount of experience and valuable knowledge from all different kinds of fields throughout my projects/work.</div>
       </div>
     }
     />
