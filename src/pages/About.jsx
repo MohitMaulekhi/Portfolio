0ade3391839e3931f1f9684eca1f07e2f7741b52
Mohit Maulekhi <mohitmaulekhi312@gmail.com>
2024-03-03 15:53:01 +0530

    Changes in text size

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -7,7 +7,7 @@ function About() {
   return (
     <Container element={
       
-      <div className="py-[5vh] mx-[2vw] flex justify-evenly items-center w-[100vw]">
+      <div className="py-[5vh] mx-[2vw] flex justify-evenly mt-[5vh]  w-[100vw]">
         <link href="https://fonts.googleapis.com/css2?family=Blinker:wght@100;200;300;400;600;700;800;900&family=Ubuntu:ital,wght@0,300;0,400;0,500;0,700;1,300;1,400;1,500;1,700&display=swap" rel="stylesheet"/>
         <div className="">
           <div className="text-[5vmin] font-extrabold  mb-[5vh]" style={{fontFamily:'Blinker'}}>About me...</div>
@@ -17,10 +17,10 @@ function About() {
             A devloper who codes in dark to avoid <span className="text-blue-700">bugs</span>.
           </div>
         </div>
-        <div className={`${theme=='dark'?"bg-white":"bg-black"} h-[75vh] w-[2vw] max-w-0.5`}></div>
-        <div className="max-w-[50vw]  bg-slate-400 bg-opacity-35 rounded-3xl flex flex-col justify-evenly p-8"  style={{fontFamily: "Ubuntu"}}>
+        <div className={`${theme=='dark'?"bg-white":"bg-black"}  w-[2vw] max-w-0.5`}></div>
+        <div className="max-w-[50vw]  bg-slate-400 bg-opacity-35 rounded-3xl flex flex-col justify-start  p-6 text-[2.5vmin]"  style={{fontFamily: "Ubuntu"}}>
           <p> I am pursuing software engineer from Netaji Subhas University of Technology. I enjoy creating things that live on the internet, whether that be websites, applications, or anything in between. I have been learning web development for a year now while studying at the university and I have manage to gain a decent amount of experience and valuable knowledge from all different kinds of fields throughout my projects/work.</p>
-          <br/>
+
           <p> I have also a great interest in Data structures and algorithms. I have completed moe than 200 questions in Gekks for Geeks and completed more than 70 questions in Leetcode.</p>
         </div>
       </div>
